ff0841550bc0baf047c594e18a1ad4cc21ec4f3d
kevin128tv-afk <kevin128tv@gmail.com>
2026-08-05 23:00:29 +0900

    Update service-worker.js

--- client/service-worker.js
+++ client/service-worker.js
@@ -1,7 +1,42 @@
+const CACHE_NAME = 'blung-v1';
+const urlsToCache = [
+  '/',
+  '/login.html',
+  '/chat.html',
+  '/manifest.json',
+  '/icon.png'
+];
+
 self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Installed');
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
-  // 웹 앱 네트워크 요청 패스스루
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request).catch(() => {
+        return caches.match('/login.html');
+      });
+    })
+  );
 });
